89bddcaa870fdb63bc17d805234a301c9de145e3
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-09-28 11:03:56 +0200

    first commit

---
+++ Lab3.java
@@ -0,0 +1,18 @@
+public class Lab3{
+    public static void main(String []args){
+        lab3();
+    }
+
+   public static void lab3(){
+/*
+ Skapa en variabel String name med ditt namn
+b. Skapa en int age med din ålder.
+c. Skriv sedan ut Jag heter Kalle (innehållet i name) och är 27(innehållet i age) år.
+ */
+
+        String name  = "Stefan";
+        int age = 51;
+        System.out.println("Jag heter " + name + " och är "+ age + " år");
+
+    }
+}
