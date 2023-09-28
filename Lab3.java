fdceadb4151566a781ab3700b6dee6bf71357394
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-09-28 11:15:55 +0200

    423423423

--- Lab3.java
+++ Lab3.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class Lab3{
     public static void main(String []args){
+        lab4();
         lab3();
     }
+
+    public static void lab4(){
+/*a. Skriv ut Skriv in ditt förnamn: . Ta emot värdet i en variabel
+b. Låt markören vänta på din inmatning på samma rad.
+c. Gör på samma sätt med efternamnet.
+d. Skriv sedan ut namnen i omvänd ordning.
+e. Se till att resultatet ser ut så här. */        
+        Scanner scanner = new Scanner(System.in);
+        //Matar in
+
+        System.out.print("Skriv in ditt förnamn:");
+        String forNamn = scanner.nextLine();
+
+        System.out.print("Skriv in ditt efternamn:");
+        String efterNamn = scanner.nextLine();
+        System.out.println("Du heter:" + efterNamn + ", " + forNamn);
+
+
+        
+
+    } 
+
+
+
 
    public static void lab3(){
 /*
